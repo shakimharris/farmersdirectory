ebc4cdd4ae8bbd6dd18cd01f8e51b0ee4f66d067
amenraprod <amenraprod@gmail.com>
2018-12-29 22:35:00 -0500

    added plugin

---
+++ wp-content/plugins/restaurantplugin.php
@@ -0,0 +1,17 @@
+<?php 
+*/
+Plugin Name: Restaurant Plugin & Taxonomies
+Plugin URI:
+Description: Adds Custom Post Types To Site
+Version: 1.0
+Author: Shakim Harris
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+
+
+
+
+?>
